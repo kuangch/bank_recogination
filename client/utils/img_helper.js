b53430c2ccc06f1736fdd746b17a37f66e841224
kuangch <kuangch_8017@qq.com>
2018-01-13 18:20:10 +0800

    function completed

--- client/utils/img_helper.js
+++ client/utils/img_helper.js
@@ -1,13 +1,24 @@
-var toBase64 = function(imgFile, callBack, type) {
-  
-  var reader = new FileReader()
-  // var base64 = wx.arrayBufferToBase64(imgFile)
-  // reader.readAsDataURL(file, { type: "image/jpeg" })
-  reader.onload = function(){
-    //回调函数
-    callBack && callBack(reader.result ? reader.result.replace("data:image/jpeg;base64,", ""): '');
-  }
-  
+var toBase64 = function (imgFile, callBack, type) {
+
+  //通过微信请求的方式获取图片的 arraybuffer
+  wx.request({
+    url: imgFile.path,
+    responseType: "arraybuffer",
+    success(res) {
+      console.log(res.data.lenght)
+      try{
+        var base64 = wx.arrayBufferToBase64(res.data) 
+        callBack && callBack(base64 ? base64 : '')
+      }catch(e){
+        console.log(e)
+        callBack(-1)
+      }
+    },
+    fail(err) {
+      callBack(-1)
+    }
+  })
+
 }
 
-module.exports = { toBase64}
+module.exports = { toBase64 }
